029734891559b06a855be6864f4f17e40419f7ad
sangam-builds <thesangamgupta@gmail.com>
2026-04-25 13:26:50 +0530

    fixed some route issues

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -1,7 +1,16 @@
+const path = require("path")
 const { sendError } = require("../utils/response.utils.js")
 
 const notFoundHandler = (req, res) => {
-	return sendError(res, 404, "Route not found")
+	if (req.accepts("html") && !req.path.startsWith("/api")) {
+		return res.status(404).sendFile(path.join(__dirname, "..", "..", "public", "404.html"))
+	}
+
+	if (req.accepts("json") || req.path.startsWith("/api")) {
+		return sendError(res, 404, "Route not found")
+	}
+
+	return res.status(404).type("txt").send("Route not found")
 }
 
 const errorHandler = (err, req, res, next) => {
